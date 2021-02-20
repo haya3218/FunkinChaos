e16d51302370b99d55c711e016d799faa26d22c5
Kitl <74699483+The-SGPT@users.noreply.github.com>
2021-02-20 15:22:51 +0800

    h

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -129,11 +129,6 @@ class MainMenuState extends MusicBeatState
 				changeItem(1);
 			}
 
-			if (controls.BACK)
-			{
-				FlxG.switchState(new TitleState());
-			}
-
 			if (controls.ACCEPT)
 			{
 				if (optionShit[curSelected] == 'donate')
